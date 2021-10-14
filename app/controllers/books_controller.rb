6ebcbcc725ad1c7dc48b99505b1a601367a1443d
sattaku1538 <choose.chose.chosen@gmail.com>
2021-10-14 08:47:20 +0000

    Update show, edit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,27 +1,50 @@
 class BooksController < ApplicationController
   def index
     @booksshow = Book.all
-    @bookscreate = Book.new
-  end
-  
-  def show
+    @book = Book.new
+    
   end
   
   def create
-    books = Book.new(books_params)
-    books.save
-    redirect_to '/show'
+    @book = Book.new(book_params)
+    if @book.save
+    flash[:alert] = "Book was successfully created."
+    redirect_to book_path(@book.id)
+    else
+    @booksshow = Book.all
+    render :index
+    
+    end
   end
-
+  
+  def show
+    @book = Book.find(params[:id])
+  end  
+  
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    flash[:alert] = "Book was successfully update."
+    redirect_to book_path(@book)
+    else
+    render :edit
+    end
   end
 
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path(book)
+    flash[:alert] = "Book was successfully destroyed."
   end
   
   private
-  def books_params
+  def book_params
     params.require(:book).permit(:title,:body)
   end
-
+  
 end
